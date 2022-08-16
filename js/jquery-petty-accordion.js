1a2ec803e81dee378b80347c70f60892605a3953
Sharaiz Khan <sharaizkhan78@gmail.com>
2022-08-16 22:20:36 +0500

    Contact left

---
+++ js/jquery-petty-accordion.js
@@ -0,0 +1,14 @@
+const acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("activefaq");
+    const panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
